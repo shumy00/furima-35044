4cbd51fed5f8c7b95d58f8f2308b9a83896d27ce
shumy00 <syamu0855@gmail.com>
2021-04-12 16:39:55 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,6 @@ RSpec.describe User, type: :model do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
-        binding.pry
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it 'passwordが確認用と一致していないと登録できない' do
