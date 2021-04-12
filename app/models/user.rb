74a79085ee65c82b9891fc7291dd14429679b6e1
shumy00 <syamu0855@gmail.com>
2021-04-12 12:24:40 +0900

    バリデーションをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
- validates :password,              format:{with:/\A[a-z0-9]+\z/i}
- validates :password_confirmation, format:{with:/\A[a-z0-9]+\z/i}
- validates :nickname, presence: true
- validates :first_name, presence: true,      format:{with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
- validates :last_name, presence: true,       format:{with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
- validates :first_kana_name, presence: true, format:{with:/\A[ァ-ヶー]+\z/}
- validates :last_kana_name, presence: true,  format:{with:/\A[ァ-ヶー]+\z/}
- validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format:{with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format:{with:/\A[ァ-ヶー]+\z/} do
+      validates :first_kana_name
+      validates :last_kana_name
+    end
+  end
+  with_options format:{with:/\A[a-z0-9]+\z/i} do
+    validates :password
+    validates :password_confirmation
+  end
 end
