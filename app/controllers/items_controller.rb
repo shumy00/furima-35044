ebc986fe02047885954324b0c7dea9492debaaf1
shumy00 <syamu0855@gmail.com>
2021-04-15 17:15:26 +0900

    createアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    
+
   end
 
   def new
