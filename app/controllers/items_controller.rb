d52ceefdd0c5b7c492d50e9cf7f198f692a9263c
shumy00 <syamu0855@gmail.com>
2021-04-17 17:37:44 +0900

    記述の簡略化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:edit]
-  before_action :move_to_index, only: [:edit, :updata]
+  before_action :set_item, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -29,7 +29,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
